ba2754705958d19a7dc9dddced740e588c854618
StaticYolt <126204653+StaticYolt@users.noreply.github.com>
2025-05-03 22:56:05 -0400

    initial decent working DailyBackup

--- DailyBackup/DailyBackup.sh
+++ DailyBackup/DailyBackup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "hello world"
+backup_folder=$1
+archive=$2
+echo "${archive:-"${1}"}"
+backup_name="$(date +"%Y-%m-%d,%H-%M-%S")"
+if [[ -d "$backup_folder" ]]; then
+	archive_path="${archive:-"${1}/archive"}"
+	mkdir -p "$archive_path"
+	$(cd "$archive_path"; tar -cvf "$backup_name" "$backup_folder" > "log_"$backup_name".txt" 2>&1) 
+fi
