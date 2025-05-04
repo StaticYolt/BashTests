c656fa3fee85aa45372482d445099d304202e453
StaticYolt <126204653+StaticYolt@users.noreply.github.com>
2025-05-03 23:06:50 -0400

    chat and improved version of DailyBackup

--- DailyBackup/DailyBackup.sh
+++ DailyBackup/DailyBackup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 backup_folder=$1
-archive=$2
-echo "${archive:-"${1}"}"
+archive=${2:-"$1/archive"}
 backup_name="$(date +"%Y-%m-%d,%H-%M-%S")"
 if [[ -d "$backup_folder" ]]; then
-	archive_path="${archive:-"${1}/archive"}"
-	mkdir -p "$archive_path"
-	$(cd "$archive_path"; tar -cvf "$backup_name" "$backup_folder" > "log_"$backup_name".txt" 2>&1) 
+	mkdir -p "$archive"
+	cd "$archive"
+	tar -cvf "$backup_name.tar" "$backup_folder" > "log_"$backup_name".txt" 2>&1)
+	if [[ $? -eq 0 ]]; then
+		echo "Backup Complete at $(date)"
+	else
+		echo "Backup failed, please check: $archive/log_$backup_name.txt"
+	fi 
+else
+	echo "Error: '$backup_folder' does not exits"
+	exit 1
 fi
+
